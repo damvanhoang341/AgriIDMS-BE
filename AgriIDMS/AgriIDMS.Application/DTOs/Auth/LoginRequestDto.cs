71aa41a0da1efdea153ffc57a46237a00ecde0cc
DVHoang <damvanhoang341@gmail.com>
2026-01-20 00:19:45 +0700

    add validation annotations to auth DTOs

--- AgriIDMS/AgriIDMS.Application/DTOs/Auth/LoginRequestDto.cs
+++ AgriIDMS/AgriIDMS.Application/DTOs/Auth/LoginRequestDto.cs
@@ -1,16 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AgriIDMS.Application.DTOs.Auth
 {
-    public record LoginRequestDto(string UserNameOrEmail, string Password);
-    public record LogoutRequestDto(string RefreshToken);
+    public class LoginRequestDto
+    {
+        [Required(ErrorMessage = "Username or Email is required")]
+        [StringLength(256,MinimumLength = 3, ErrorMessage = "Username or Email must be between 3 and 256 characters")]
+        public string UserNameOrEmail { get; set; } = string.Empty;
 
-    public record AuthResponseDto(string AccessToken,string RefreshToken,string UserId,string UserName,IList<string> Roles);
+        [Required(ErrorMessage = "Password is required")]
+        [MinLength(8, ErrorMessage = "Password must be at least 8 characters long")]
+        public string Password { get; set; } = string.Empty;
 
-    public record RefreshRequestDto(string RefreshToken);
+    }
 
+    public class LogoutRequestDto
+    {
+        [Required(ErrorMessage = "Refresh Token is required")]
+        public string RefreshToken { get; set; } = string.Empty;
+    }
+
+    public class RefreshRequestDto
+    {
+        [Required(ErrorMessage = "RefreshToken is required.")]
+        public string RefreshToken { get; set; } = default!;
+    }
+
+    public class AuthResponseDto
+    {
+        [Required(ErrorMessage = "AccessToken is required.")]
+        public string AccessToken { get; set; } = default!;
+
+        [Required(ErrorMessage = "RefreshToken is required.")]
+        public string RefreshToken { get; set; } = default!;
+
+        [Required(ErrorMessage = "UserId is required.")]
+        public string UserId { get; set; } = default!;
+
+        [Required(ErrorMessage = "UserName is required.")]
+        public string UserName { get; set; } = default!;
+
+        [Required(ErrorMessage = "Roles is required.")]
+        [MinLength(1, ErrorMessage = "Roles must contain at least 1 role.")]
+        public IList<string> Roles { get; set; } = new List<string>();
+    }
 }
