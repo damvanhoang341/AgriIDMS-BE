47dec5274f61124a198f898a2a1e81505d2710f0
DVHoang <damvanhoang341@gmail.com>
2026-02-06 00:29:38 +0700

    add product in Domain.Entities

---
+++ AgriIDMS/AgriIDMS.Domain/Enums/ProductEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgriIDMS.Domain.Enums
+{
+    public enum ProductStatus
+    {
+        Inactive = 0,
+        Active = 1,
+        OutOfStock = 2
+    }
+}
