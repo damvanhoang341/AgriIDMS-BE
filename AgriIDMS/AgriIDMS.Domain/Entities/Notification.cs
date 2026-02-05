687fe9d33c5e6dbc6be7def9d42c0d4962ce8e48
DVHoang <damvanhoang341@gmail.com>
2026-02-06 01:21:33 +0700

    add Notification in Domain.Entities connect ApplicationUser many to many

---
+++ AgriIDMS/AgriIDMS.Domain/Entities/Notification.cs
@@ -0,0 +1,21 @@
+﻿using AgriIDMS.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgriIDMS.Domain.Entities
+{
+    public class Notification
+    {
+        public int Id { get; private set; }
+        public NotificationType Type { get; private set; }
+        public string Message { get; private set; } = null!;
+        public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
+
+        public ICollection<UserNotification> UserNotifications { get; private set; }
+            = new List<UserNotification>();
+    }
+
+}
