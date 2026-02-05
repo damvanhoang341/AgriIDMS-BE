e556c585ade07f6617f2828870d8edac13135b82
DVHoang <damvanhoang341@gmail.com>
2026-02-05 14:24:22 +0700

    add Pagination

---
+++ AgriIDMS/AgriIDMS.Application/Pagination/PaginationExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgriIDMS.Application.Pagination
+{
+    public static class PaginationExtensions
+    {
+        public static async Task<PaginationResult<T>> ToPagedResultAsync<T>(
+            this IQueryable<T> query,
+            int pageIndex,
+            int pageSize)
+        {
+            pageIndex = pageIndex < 1 ? 1 : pageIndex;
+            pageSize = pageSize <= 0 ? 10 : pageSize;
+            pageSize = pageSize > 100 ? 100 : pageSize;
+
+            var totalItems = await query.CountAsync();
+
+            var items = await query
+                .Skip((pageIndex - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+
+            return new PaginationResult<T>
+            {
+                PageIndex = pageIndex,
+                PageSize = pageSize,
+                TotalItems = totalItems,
+                TotalPages = (int)Math.Ceiling(
+                    totalItems / (double)pageSize),
+                Items = items
+            };
+        }
+    }
+}
---
+++ AgriIDMS/AgriIDMS.Application/Pagination/PaginationRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgriIDMS.Application.Pagination
+{
+    public class PaginationRequest
+    {
+        public int PageIndex { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+    }
+
+}
