07db5263d57ba9603c238f2fa15373a35e2920f1
DVHoang <damvanhoang341@gmail.com>
2026-03-07 22:40:56 +0700

    update code

--- AgriIDMS/AgriIDMS.Domain/Enums/PurchaseOrderStatus.cs
+++ AgriIDMS/AgriIDMS.Domain/Enums/PurchaseOrderStatus.cs
@@ -8,11 +8,11 @@ namespace AgriIDMS.Domain.Enums
 {
     public enum PurchaseOrderStatus
     {
-        Draft = 0,        // Đang tạo, chưa gửi
-        Pending = 1,      // Đã gửi NCC, chờ xác nhận
-        Approved = 2,     // Đã duyệt
-        PartiallyReceived = 3, // Đã nhận 1 phần
-        Completed = 4,    // Đã nhận đủ
-        Cancelled = 5     // Đã hủy
+        //Draft = 0,        // Đang tạo, chưa gửi
+        Pending = 0,      // Đã gửi NCC, chờ xác nhận
+        Approved = 1,     // Đã duyệt
+        //PartiallyReceived = 3, // Đã nhận 1 phần
+        Completed = 2,    // Đã nhận đủ
+        Cancelled = 3     // Đã hủy
     }
 }
