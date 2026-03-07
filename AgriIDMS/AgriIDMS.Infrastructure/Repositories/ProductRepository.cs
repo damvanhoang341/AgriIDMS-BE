2ca9939234d295e118718c89a3dd46551d33e502
Thanh <cnguyenphuongthanh@gmail.com>
2026-03-08 01:21:26 +0700

    add repo + inteface : product and product variant

--- AgriIDMS/AgriIDMS.Infrastructure/Repositories/ProductRepository.cs
+++ AgriIDMS/AgriIDMS.Infrastructure/Repositories/ProductRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AgriIDMS.Domain.Entities;
+using AgriIDMS.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,41 @@ using System.Threading.Tasks;
 
 namespace AgriIDMS.Infrastructure.Repositories
 {
-    internal class ProductRepository
+    public class ProductRepository : IProductRepository
     {
+        private readonly AppDbContext _context;
+        public ProductRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task AddProductAsync(Product product)
+        {
+            await _context.Products.AddAsync(product);
+        }
+
+        public async Task<IEnumerable<Product>> GetAllProductsAsync()
+        {
+            return await _context.Products
+                .AsNoTracking()
+                .ToListAsync();
+        }
+
+        public async Task<Product?> GetProductByIdAsync(int productId)
+        {
+            return await _context.Products
+                .AsNoTracking()
+                .FirstOrDefaultAsync(p => p.Id == productId);
+        }
+
+        public Task UpdateProductAsync(Product product)
+        {
+            _context.Products.Update(product);
+            return Task.CompletedTask;
+        }
+
+        public async Task<bool> ExistsByNameAsync(string name)
+        {
+            return await _context.Products.AnyAsync(x => x.Name == name);
+        }
     }
 }
