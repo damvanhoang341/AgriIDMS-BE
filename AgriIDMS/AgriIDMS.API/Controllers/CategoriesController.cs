199bc7a3c756b17ea832f58fe49344717c649846
DucDMHE181735 <90913757+quynguyen07012003@users.noreply.github.com>
2026-03-08 11:38:56 +0700

    update code CategoryController  + DI

--- AgriIDMS/AgriIDMS.API/Controllers/CategoriesController.cs
+++ AgriIDMS/AgriIDMS.API/Controllers/CategoriesController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AgriIDMS.Application.DTOs.Category;
+using AgriIDMS.Application.Interfaces;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AgriIDMS.API.Controllers
@@ -7,5 +9,47 @@ namespace AgriIDMS.API.Controllers
     [ApiController]
     public class CategoriesController : ControllerBase
     {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _categoryService.GetAllAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            return Ok(await _categoryService.GetByIdAsync(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateCategoryRequest request)
+        {
+            var id = await _categoryService.CreateAsync(request);
+
+            return Ok(id);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, UpdateCategoryRequest request)
+        {
+            await _categoryService.UpdateAsync(id, request);
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _categoryService.DeleteAsync(id);
+
+            return Ok();
+        }
     }
 }
