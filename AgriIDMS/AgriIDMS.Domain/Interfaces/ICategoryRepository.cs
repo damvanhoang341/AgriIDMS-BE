35e12402d90bd226490e05510621b5c554cf2995
DucDMHE181735 <90913757+quynguyen07012003@users.noreply.github.com>
2026-03-08 11:30:58 +0700

    update code ICategoryRepositopry

--- AgriIDMS/AgriIDMS.Domain/Interfaces/ICategoryRepository.cs
+++ AgriIDMS/AgriIDMS.Domain/Interfaces/ICategoryRepository.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AgriIDMS.Domain.Entities;
 
 namespace AgriIDMS.Domain.Interfaces
 {
-    internal class ICategoryRepository
+    public interface ICategoryRepository
     {
+        Task<Category?> GetByIdAsync(int id);
+
+        Task<IEnumerable<Category>> GetAllAsync();
+
+        Task<bool> ExistsByNameAsync(string name);
+
+        Task AddAsync(Category category);
+
+        void Update(Category category);
     }
 }
