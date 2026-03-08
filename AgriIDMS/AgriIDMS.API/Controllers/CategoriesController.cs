c0d5f96ac6396cbac48eb988a012103143b31e28
DucDMHE181735 <90913757+quynguyen07012003@users.noreply.github.com>
2026-03-08 11:38:03 +0700

    add class controller CategoriesController

---
+++ AgriIDMS/AgriIDMS.API/Controllers/CategoriesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgriIDMS.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+    }
+}
