6cf1fa407d8dea3d0c7aaa9dea055fe71b44545e
DucDMHE181735 <90913757+quynguyen07012003@users.noreply.github.com>
2026-03-11 10:39:19 +0700

    update code order repository

--- AgriIDMS/AgriIDMS.Domain/Interfaces/IOrderRepository.cs
+++ AgriIDMS/AgriIDMS.Domain/Interfaces/IOrderRepository.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using AgriIDMS.Domain.Entities;
 using System.Threading.Tasks;
 
 namespace AgriIDMS.Domain.Interfaces
 {
-    internal class IOrderRepository
+    public interface IOrderRepository
     {
+        Task AddAsync(Order order);
+
+        Task<Order?> GetByIdWithDetailsAsync(int id);
     }
 }
+
