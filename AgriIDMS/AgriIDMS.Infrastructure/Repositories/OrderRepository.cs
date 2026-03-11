48824bfb08fda59d54d44703b258ea5671a4822a
DucDMHE181735 <90913757+quynguyen07012003@users.noreply.github.com>
2026-03-11 10:40:39 +0700

    update code OrderReposittory

--- AgriIDMS/AgriIDMS.Infrastructure/Repositories/OrderRepository.cs
+++ AgriIDMS/AgriIDMS.Infrastructure/Repositories/OrderRepository.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using AgriIDMS.Domain.Entities;
+using AgriIDMS.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 
 namespace AgriIDMS.Infrastructure.Repositories
 {
-    internal class OrderRepository
+    public class OrderRepository : IOrderRepository
     {
+        private readonly AppDbContext _context;
+
+        public OrderRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(Order order)
+        {
+            await _context.Orders.AddAsync(order);
+        }
+
+        public async Task<Order?> GetByIdWithDetailsAsync(int id)
+        {
+            return await _context.Orders
+                .Include(o => o.Details)
+                .ThenInclude(d => d.ProductVariant)
+                .FirstOrDefaultAsync(o => o.Id == id);
+        }
     }
 }
+
