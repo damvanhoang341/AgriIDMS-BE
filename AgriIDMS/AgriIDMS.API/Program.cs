0c4d1096c573a53eb0009bf1a504a66bc625e420
DVHoang <damvanhoang341@gmail.com>
2026-04-13 22:59:12 +0700

    modify cors url fe

--- AgriIDMS/AgriIDMS.API/Program.cs
+++ AgriIDMS/AgriIDMS.API/Program.cs
@@ -15,7 +15,8 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowReact",policy =>
         {
             policy.WithOrigins("http://localhost:5173",
-                "https://<https://agreeable-pebble-0c3796b00.7.azurestaticapps.net>.azurestaticapps.net")
+                "https://agreeable-pebble-0c3796b00.7.azurestaticapps.net"
+                )
                   .AllowAnyHeader()
                   .AllowAnyMethod()
                   .AllowCredentials();
