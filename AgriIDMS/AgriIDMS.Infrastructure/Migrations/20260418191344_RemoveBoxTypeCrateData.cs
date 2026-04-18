1deca45016ada2ec62127ab9fcbd3de0510b845e
DVHoang <damvanhoang341@gmail.com>
2026-04-19 03:01:03 +0700

    delete box type sot

---
+++ AgriIDMS/AgriIDMS.Infrastructure/Migrations/20260418191344_RemoveBoxTypeCrateData.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AgriIDMS.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveBoxTypeCrateData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // BoxType.Crate (=4) đã bỏ khỏi enum. Chuẩn hóa dữ liệu cũ trước khi deploy code mới.
+            migrationBuilder.Sql(
+                """
+                UPDATE [Boxes] SET [BoxType] = 0 WHERE [BoxType] = 4;
+                DELETE FROM [BoxTypeSpecs] WHERE [BoxType] = 4;
+                """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // Không khôi phục giá trị enum đã xóa.
+        }
+    }
+}
